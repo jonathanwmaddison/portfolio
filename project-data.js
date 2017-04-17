d97a59989172a2f08fba8c8c5d88d7fb1aff210b
Jonathan Maddison <jonathanwmaddison@gmail.com>
2017-04-17 11:30:31 -0700

    add hover over for portfolio page

--- project-data.js
+++ project-data.js
@@ -24,7 +24,8 @@ var Projects = {
     projectData:  [
         {
             id: 'rep-finder',
-            title: 'Know Your Rep',
+            title: 'Know Your Official',
+            shortSummary: 'Know your Official is a web app where you can look up your local officials and learn about them through multiple sources.',
             summary: 'This is a personal project that I thought up when I struggled to find out who my elected officials were and what their positions are. I decided to build a simple web app to help address this problem. I used a variety of data sources and public APIs to build the "Know Your Official" web app. I addressed the following user stories:',
             userStories: [
                 "I can input my street address and retrieve a list of all of my officials from the national, state to local level.",
@@ -51,6 +52,7 @@ var Projects = {
         {
             id: 'game-of-life',
             title: 'Game Of Life',
+            shortSummary: 'The Game of Life is a simulation of a very simple cellular network where each cell lives or dies based on the status of its neighbors.',
             summary: 'The Game of Life is a simulation that I developed as part of freeCodeCamp\'s data visualization certificate. It is a model of a simple system where each lives or dies based on the status of their neighbors. I addressed the following user stories:',
             userStories: [
                 "When I first arrive at the game, it will randomly generate a board and start playing",
@@ -72,6 +74,7 @@ var Projects = {
             id: "rising-circles",
             title: "Rising Circles: Drum Machine",
             summary: "Rising Circles started off as a clone of another web app called patatap and evolved into a drum machine. Two libraries were helpful in building this project, Howler JS and Paper JS.",
+            shortSummary: 'Rising Circles is a drum machine where you can make colorful beats',
             userStories: ["I can create a simple drum beat by pressing keys on my keyboard", "I can change the tempo of the beat", "I can change the soundbank and try out new sounds", "I can pause and rest the drum machine"],
             tools: ["Paper JS", "Howler", "Javascript", "CSS", "HTML"],
             tags: ["Javascript", "HTML", "CSS", "Javascript Libraries"],
@@ -82,6 +85,7 @@ var Projects = {
         {
             id: "dungeon-crawler",
             title: "Pixel Labryinth",
+            shortSummary: 'Pixel Labryinth is a randomly generated dungeon crawler I built for freeCodeCamp',
             summary: "I built Pixel Labryinth as part of freeCodeCamp's challenge. The goal of Pixel Labryinth is to explore and survive a randomly generated labryinth. The most interesting aspect of this project was writing the map generating algorithm. Generating a random map is easy, but making the map feel maze took some more creativity. React was a useful tool here because it allowed to manage state in one class, and then pass down updates into rendering components",
             userStories: [
                 "I have health, a level, and a weapon",
@@ -101,6 +105,7 @@ var Projects = {
         {
             id: "simon",
             title: "Simon",
+            shortSummary: "Simon is a early pattern matching/rhythm game from the 80's. This web app was built as part of freeCodeCamp's Front End Certification Program",
             summary: "Simon is a early pattern matching/rhythm game from the 80's. This web app was built as part of freeCodeCamp's Front End Certification Program",
             userStories: [
             
@@ -119,7 +124,15 @@ var Projects = {
             github: "https://github.com/jonathanwmaddison/Simon-game",
             webpage: "https://jonathanwmaddison.github.io/Simon-game/index.html",
         },
-        {
+        
+        ],
+        findProject: function (id) {
+            return this.projectData.filter((project)=> project.id === id)[0]
+        }
+}
+/*
+Projects I'm not including
+{
             id: "recipe-box",
             title: "Recipe Box",
             summary: "A very simple app for keeping track of recipes and ingredients, built with React",
@@ -137,10 +150,5 @@ var Projects = {
             github: "https://github.com/jonathanwmaddison/recipe-box",
             webpage: "https://jonathanwmaddison.github.io/recipe-box/build/index.html",
         }
-        ],
-        findProject: function (id) {
-            return this.projectData.filter((project)=> project.id === id)[0]
-        }
-}
-
+*/
 module.exports = Projects
